1fb02d37423e2b15ae2c7735e269da268a814958
Nathan <pythoniszen@gmail.com>
2024-12-27 03:05:46 -0800

    Implement shape struct and templates; create and render current shape; handle memory allocation for current shape; add drop logic for current shape

--- matrix_break_pc/src/main.c
+++ matrix_break_pc/src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <SDL2/SDL.h>
+#include <time.h>
 
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 64
@@ -16,7 +18,85 @@
 #define MATRIX_HEIGHT 24
 #define BLOCK_SIZE 4
 
+/* Shapes and rotation state */
+typedef struct {
+  uint8_t shape[4][4];
+  int x, y;
+  int rotation;
+} Shape;
+
+/* Shape templates 3d array */
+const uint8_t SHAPES[][4][4] = {
+  {
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0},
+    {1,1,1,1}
+  },
+  {
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0},
+    {1,0,0,0}
+  },
+  {
+    {0,0,0,0},
+    {0,0,0,0},
+    {1,1,0,0},
+    {1,1,0,0}
+  }
+};
+
+/* Create shape function */
+Shape* create_shape() {
+  Shape* new_shape = malloc(sizeof(Shape));
+
+  /* Init x, y, rotation */
+  new_shape->x = 30;
+  new_shape->y = 6;
+  new_shape->rotation = 0;
+
+  /* Pick random shape */
+  int rand_temp = rand() % 3;
+
+  /* Copy shape data from template */
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 4; j++) {
+      new_shape->shape[i][j] = SHAPES[rand_temp][i][j];
+    }
+  }
+
+  return new_shape;
+}
+
+void move_shape(Shape* shape, int dx, int dy) {
+  shape->x += (dx * BLOCK_SIZE);
+  shape->y += (dy * BLOCK_SIZE);
+}
+
+/* Render shape function */
+void render_shape(SDL_Renderer* renderer, const Shape* shape) {
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+  for(int i = 0; i < 4; i++) {
+    for(int j = 0; j < 4; j++) {
+      if(shape->shape[i][j]) {
+        SDL_Rect block = {
+          shape->x + (j * BLOCK_SIZE),
+          shape->y + (i * BLOCK_SIZE),
+          BLOCK_SIZE,
+          BLOCK_SIZE
+        };
+        SDL_RenderFillRect(renderer, &block);
+      }
+    }
+  }
+}
+
 int main() {
+  /* Seed rng with current time */
+  srand(time(NULL));
+
   /* Initialize SDL */
   SDL_Init(SDL_INIT_VIDEO);
 
@@ -31,6 +111,12 @@ int main() {
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
   SDL_RenderSetLogicalSize(renderer, GAME_WIDTH, GAME_HEIGHT);
 
+  /* Create current playing shape and state variables */
+  Shape* current_shape = create_shape();
+  unsigned int last_drop_time = SDL_GetTicks();
+  const int DROP_SPEED = 500;
+
+  /* Create SDL event for polling and variable for game loop on/off */
   int running = 1;
   SDL_Event event;
 
@@ -43,30 +129,37 @@ int main() {
       }
     }
 
+    /* Drop current shape */
+    unsigned int current_time = SDL_GetTicks();
+    if(current_time - last_drop_time > DROP_SPEED) {
+      move_shape(current_shape, 0, 1);
+      last_drop_time = current_time;
+    }
+
     /* Clear window to black */
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
     
-    /* Render border */
+    /* Render border */ 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_Rect outer_border = { 0, 0, 64, MATRIX_HEIGHT * BLOCK_SIZE + 4 };
-    SDL_RenderDrawRect(renderer, &outer_border);
-    SDL_Rect inner_border = { 1, 1, 62, MATRIX_HEIGHT * BLOCK_SIZE + 2 };
-    SDL_RenderDrawRect(renderer, &inner_border);
-
-    /* Render block: x, y, width, height */
-    SDL_Rect block = {
-      30, 
-      6,
-      4,  
-      4 
+    SDL_Rect border = {
+      0,
+      0,
+      16 * BLOCK_SIZE,
+      MATRIX_HEIGHT * BLOCK_SIZE
     };
-    SDL_RenderFillRect(renderer, &block);
+    SDL_RenderDrawRect(renderer, &border);
 
+    /* Render current shape */
+    render_shape(renderer, current_shape);
+    
     /* Present renderer */
     SDL_RenderPresent(renderer);
   } 
   
+  /* Frees working shape pointer from memory */
+  free(current_shape);
+
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
